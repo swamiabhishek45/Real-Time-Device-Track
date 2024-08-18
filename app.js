6206417b0b3f4a047ff375d04bc2c17056bd8e63
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-08-19 00:40:59 +0530

    add

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const socketio = require("socket.io");
+const path = require("path");
+const http = require("http");
 
-app.get('/', function(req, res){
-    res.send("hi")
-})
+// Express setup
+const app = express();
+const port = 3000;
+
+// Socket.io setup
+const server = http.createServer(app);
+const io = socketio(server);
+
+// ejs setup
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", function (socket) {
+    socket.on("send-location", function (data) {
+        io.emit("receive-location", { id: socket.id, ...data });
+    });
+
+    socket.on("disconnect", function () {
+        io.emit("user-disconnected", socket.id);
+    });
+    console.log("Connected!!");
+});
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+server.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
